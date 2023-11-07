dfbbe293f3f4010324cb16666f62a56080a53563
unknown <kalpitraorane47@gmail.com>
2023-11-07 11:52:04 +0530

    Q&A

---
+++ ReactQ&A.js
@@ -0,0 +1,55 @@
+// Core Concepts:
+// What is React.js?
+// Explain the Virtual DOM in React.
+// What is JSX? How is it different from HTML?
+// What are React components?
+// What is the difference between state and props?
+// What are controlled and uncontrolled components in React?
+// Explain the component lifecycle in React.
+// What is the significance of keys in React lists?
+// What are React hooks? Provide examples of built-in hooks.
+
+
+// Component Architecture:
+// What is higher-order component (HOC) in React?
+// Explain render props pattern in React.
+// What are React fragments and why are they useful?
+// What is context in React and when is it used?
+
+
+// State Management:
+// What is Redux? Why might you use it in a React app?
+// Explain the Flux architecture and its components.
+// What are Redux actions, reducers, and stores?
+// What is the difference between Redux and React's context API for state management?
+// What are Redux middleware? Why are they used?
+
+
+// Routing and Navigation:
+// What is React Router? How do you implement routing in a React application?
+// Explain the difference between <Link> and a href for navigation in React Router.
+// What are nested routes in React Router?
+// How do you handle 404 errors in React Router?
+
+
+// Testing and Debugging:
+// What is Jest? How is it used for testing React applications?
+// Explain the concept of snapshots in Jest testing.
+// What are the various lifecycle methods used for testing in React?
+// What are React DevTools and why are they helpful for debugging?
+
+
+// Best Practices and Optimization:
+// What are PureComponent and React.memo()?
+// Explain code splitting in React. Why is it important for performance?
+// What are React suspense and lazy loading?
+// What are keys in React and why are they important?
+// What are some performance optimization techniques in React?
+
+
+// Miscellaneous:
+// What is Next.js? How does it differ from React?
+// Explain server-side rendering (SSR) and its benefits.
+// What are hooks in React and how do they differ from classes?
+// How do you optimize React components for production?
+// Explain the concept of error boundaries in React.
